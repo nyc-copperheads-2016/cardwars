d8535d5178c714abbb1f7d9e9fe1e220de900499
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2015-12-05 17:13:28 -0500

    adds card and deck functionality

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+  def is_correct?(guess)
+    guess.downcase==answer.downcase
+  end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,14 @@
 class Deck < ActiveRecord::Base
   has_many :cards
   belongs_to :round
+
+  def load_cards
+    #takes in all the cards with the id of the current deck
+    #returns an array
+    self.cards.to_a
+  end
+  def next_card
+    #returns a random card in the deck
+    self.cards.find_by(:id = rand(self.cards.size + 1))
+  end
 end
