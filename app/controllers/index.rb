0d2ebe09cf7ccd389c812fcc92eed07e9e47eb96
courtney wiggins <courtneyrwiggins@gmail.com>
2015-12-07 06:24:39 +0000

    modifys index route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
 get '/' do
-  decks = Deck.all
-  erb :'index', locals: { available_decks: decks }
+  redirect "/index"
 end
+
+## I don't think the logic on lines 6-8 belongs in this route in particular.  I adjusted.
+  #decks = Deck.all
+  #erb :'index', locals: { available_decks: decks }
+#end
+
+  #erb :'/index'
+#end
+
