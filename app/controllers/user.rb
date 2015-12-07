8beebd5c71ffe1412f868976cb1f2a95e2076a96
James Kirkpatrick <jkirkpatrick1990@gmail.com>
2015-12-06 20:57:41 -0500

    partially fix user pathways for authentication

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,18 +1,22 @@
-get '/users' do
+# path for create
+get '/users/new' do
   erb :'/users/new'
 end
 
 post '/users' do
-  user = User.new(params[:user])
+  user = User.new(params)
   if user.save
     session[:user_id] = user.id
-    redirect "/users/#{user.id}"
+    redirect "/"
   else
     @errors = user.errors.full_messages
     erb :'/users/new'
   end
 end
 
-
+get '/users/:id' do
+  # @user = User.find_by(id: session[:user_id])
+  erb :'/users/show'
+end
 
 
--- db/migrate/20151206175856_add_password_encryption.rb
+++
@@ -1,9 +0,0 @@
-class AddPasswordEncryption < ActiveRecord::Migration
-  def change
-    add_column :username, :password_hash
-    remove_column :users, :password
-  end
-end
-
-
-
