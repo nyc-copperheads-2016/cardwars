3b8a5e0c1d02387fe878f1263c12e1d1523ebee5
Jay Yee <fmo@opmbx.org>
2015-12-06 19:45:53 -0500

    Add main functionality of game

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,6 +1,9 @@
 get '/decks/:id' do
   new_round = Round.create(user_id: session[:user_id])
   new_round.decks << Deck.find(params[:id])
-  new_card = new_round.decks.first.cards.shuffle.pop
+  new_round.decks.first.cards.each do |card|
+    card.update(is_answered_correct: false)
+  end
+  new_card = new_round.decks.last.cards.shuffle.pop
   erb :'rounds/new', locals: { round: new_round, card: new_card }
 end
--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,27 +1,28 @@
-get '/rounds/:id' do
+get '/rounds/new/:id' do
   erb :'rounds/new'
 end
 
 put '/rounds/answer/:id' do
+  correct_answer = params[:card]
   current_round = Round.find(params[:id])
-  #binding.pry
+  result = false
   if params[:user_answer].downcase == params[:card].downcase
-    current_round[:num_guesses] += 1
-    current_round[:correct_first_guess] += 1
+    current_round.update(correct_first_guess: (current_round[:correct_first_guess] + 1))
     current_round.decks.first.cards.find(params[:card_id]).update(is_answered_correct: true)
-    redirect '/rounds/#{current_round.id}'
-  else
-    current_round[:num_guesses] += 1
-    redirect '/rounds/incorrect_answer/#{current_round.id}'
+    result = true
   end
+  current_round.update(num_guesses: (current_round[:num_guesses] + 1))
+  erb :'/rounds/result', locals: { round: current_round, outcome: result, answer: correct_answer }
 end
 
-get '/rounds/:id/' do
+get '/rounds/:id' do
   current_round = Round.find(params[:id])
-  card_answer = current_round.decks.first.cards.find(params[:card_id]).answer
-  erb :'rounds/correct_answer', locals: { round: current_round, answer: card_answer }
-end
-
-get '/rounds/:id/incorrect_answer/:card_id' do
-  erb :'rounds/incorrect_answer'
+  leftovers = current_round.decks.last.cards.select { |card| card.is_answered_correct == false }
+  unless leftovers.length == 0
+    new_card = leftovers.shuffle.pop
+    erb :'rounds/new', locals: { round: current_round, card: new_card }
+  else
+    binding.pry
+    redirect '/'
+  end
 end
