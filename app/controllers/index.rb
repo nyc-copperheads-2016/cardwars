748b9ca3833540196aee1615dd976eef2a48e135
Courtney <aquarian5@users.noreply.github.com>
2015-12-07 00:19:45 -0500

    Update index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,5 @@ get '/' do
   decks = Deck.all
   erb :'index', locals: { available_decks: decks }
 end
+
+#we might want to rethink this- should redirect to the index.erb instead of putting deck logic here. Just a thought -Court
